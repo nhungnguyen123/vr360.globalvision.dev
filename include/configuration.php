a6956b60b7386a5d68f0ce7178f131bdff8b16bc
Pham Chan Nhan <pham.channhan@gmail.com>
2017-09-01 00:26:08 +0700

    upload file, create xml and db save basic

--- include/configuration.php
+++ include/configuration.php
@@ -4,8 +4,8 @@ class Vr360Configuration
 {
 	public $dbName = 'globalvision_vr360';
 	public $dbUser = 'root';
-	public $dbPassword = 'root';
-	public $dbServer = 'localhost';
+	public $dbPassword = '123123123';
+	public $dbServer = 'db';
 	public $siteName = 'Vr360 - Globalvision';
 
 	public static function getInstance()
