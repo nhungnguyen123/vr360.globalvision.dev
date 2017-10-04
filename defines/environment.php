30be1e39ed85fca1bab4315d136080a367e12861
Viet Vu <jooservices@gmail.com>
2017-10-04 19:45:06 +0700

    ^ temporary

--- defines/environment.php
+++ defines/environment.php
@@ -3,3 +3,4 @@
 defined('_VR360_EXEC') or die;
 
 define('VR360_URL_ROOT', (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]");
+define('VR360_URL_FULL', VR360_URL_ROOT . $_SERVER['REQUEST_URI']);
