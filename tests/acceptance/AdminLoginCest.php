f6db2c88d53f2aec8476854650a4275a1bc36e84
nhungnguyen123 <nhungnth19951903@gmail.com>
2017-10-22 10:24:32 +0700

    test

---
+++ tests/acceptance/AdminLoginCest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class AdminLoginCest
+{
+    public function login(AcceptanceTester $I)
+    {
+        $I->amOnPage('');
+        $I->comment('Fill Username Text Field');
+        // $I->wait(30);
+        $I->fillField(['xpath'=>"//input[@name='username']"], 'designteam');
+        $I->comment('Fill Password Text Field');
+        $I->fillField("//input[@name='password']", 'nQ-yQ?3ba');
+        $I->comment('I click Login button');
+        $I->click("//input[@name='submit']");
+        $I->waitForElement("//img[@id='logo']",30);
+        $I->wait(5);
+
+        $I->comment('I see Administrator Control Panel');
+        // $I->see('Control Panel', '.page-title');
+    }
+}
