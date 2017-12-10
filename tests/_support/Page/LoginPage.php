87aebe2bdefd6db07cfb6234d44d38887b1245e5
Viet Vu <jooservices@gmail.com>
2017-12-10 10:19:52 +0700

    ^ fix main social buttons ^ code clean up

--- tests/_support/Page/LoginPage.php
+++ tests/_support/Page/LoginPage.php
@@ -1,47 +1,66 @@
 <?php
 
 namespace Page;
+
+/**
+ * Class LoginPage
+ *
+ * @package Page
+ */
 class LoginPage
 {
- 
-    
-    public static $usernameField = ['xpath'=>"//input[@name='username']"];
-    
-    public static $passfield = ['xpath' => "//input[@name='password']"];
-    
-    public static $btnLogin = ['xpath' =>' //input[@name=\'submit\']'];
-    
-    public static $logoXpath = ['xpath' =>' //img[@id=\'logo\']'];
+	public static $usernameField = ['xpath' => "//input[@name='username']"];
 
-    public static $warningUserNameOrPassXpath = ['xpath' => '//div[@class=\'label label-warning\']'];
+	public static $passfield = ['xpath' => "//input[@name='password']"];
 
-    public static $warningUsername = ['xpath' =>'//div[@class=\'label label-default\']'];
+	public static $btnLogin = ['xpath' => ' //input[@name=\'submit\']'];
 
-    public static $warningUserNameOrPassXpathMessage = 'Invalid Username or password';
+	public static $logoXpath = ['xpath' => ' //img[@id=\'logo\']'];
 
-    public static $warningUsernameMessage = 'Invalid username';
-    
-    public static $warningInvalidPassMessage = 'Invalid password';
+	public static $warningUserNameOrPassXpath = ['xpath' => '//div[@class=\'label label-warning\']'];
 
-    public static $logoId = ['id' => 'logo'];
+	public static $warningUsername = ['xpath' => '//div[@class=\'label label-default\']'];
 
-    public static $faceIcon = ['xpath' => '//i[@id=\'social-fb\']'];
+	public static $warningUserNameOrPassXpathMessage = 'Invalid Username or password';
 
-    public static $facePage = ['xpath' =>'//i[@class=\'fb_logo img sp_ex0C8BIsLat sx_7401de\']'];
-    
-    public static $faceURL = 'https://www.facebook.com/globalvision360/';
+	public static $warningUsernameMessage = 'Invalid username';
 
-    public static $tweeticon  = ['xpath' => '//i[@id=\'social-tw\']'];
+	public static $warningInvalidPassMessage = 'Invalid password';
 
-    public static $twitterURL = 'https://twitter.com/GlobalVision360';
+	public static $logoId = ['id' => 'logo'];
 
-    public static $googleIcon = ['xpath' =>'//i[@id=\'social-gp\']'];
-    
-    public static $googleURL = 'https://plus.google.com/+GlobalVisionSwitzerland';
+	/**
+	 * @var array
+	 */
+	public static $faceIcon = ['xpath' => '//a[@id=\'social-facebook\']'];
 
-    public static $pageMain = 'http://globalvision.ch/en/home-page/';
+	/**
+	 * @var string
+	 */
+	public static $faceURL = 'https://www.facebook.com/globalvision360/';
 
+	/**
+	 * @var array
+	 */
+	public static $tweeticon = ['xpath' => '//a[@id=\'social-twitter\']'];
 
-    
-    
+	/**
+	 * @var string
+	 */
+	public static $twitterURL = 'https://twitter.com/GlobalVision360';
+
+	/**
+	 * @var array
+	 */
+	public static $googleIcon = ['xpath' => '//a[@id=\'social-googleplus\']'];
+
+	/**
+	 * @var string
+	 */
+	public static $googleURL = 'https://plus.google.com/+GlobalVisionSwitzerland';
+
+	/**
+	 * @var string
+	 */
+	public static $pageMain = 'http://globalvision.ch/en/home-page/';
 }
