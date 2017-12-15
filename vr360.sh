b41fb56e7772f60085176144ec46868a799ba479
Viet Vu <jooservices@gmail.com>
2017-12-15 11:20:33 +0700

    ^ update batch file to fetch Krpano 1.19 pr14

--- vr360.sh
+++ vr360.sh
@@ -10,11 +10,11 @@ composer clear-cache
 composer install
 
 rm -rf krpano
-wget https://krpano.com/download/files/krpano-1.19-pr13-linux64.tar.gz
-tar -xvf krpano-1.19-pr13-linux64.tar.gz
-mv ./krpano-1.19-pr13 ./krpano
+wget https://krpano.com/download/files/krpano-1.19-pr14-linux64.tar.gz
+tar -xvf krpano-1.19-pr14-linux64.tar.gz
+mv ./krpano-1.19-pr14 ./krpano
 ./krpano/INSTALL\ -\ Create\ Linux\ Desktop\ Icons.sh
 
-rm -rf krpano-1.19-pr13-linux64.tar.gz
+rm -rf krpano-1.19-pr14-linux64.tar.gz
 rm -rf ./krpano/docu
 rm -rf ./krpano/viewer/examples
